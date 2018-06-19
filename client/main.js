530b07c6b542b014c4613f17509ef5c6416c6bd2
Kevin Josey <krj166@humboldt.edu>
2018-06-19 14:44:36 -0700

    Created MyWebsite for GitHub

---
+++ client/main.js
@@ -0,0 +1,23 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+$(document).ready(function() {
+
+  $('[data-toggle="collapse"]').click(function() {
+    $('#navbarColor01').toggleClass('toggle');
+  });
+}); 
+
+Router.route('/', function () {
+  this.render('Home');
+});
+
+Router.route('about', function () {
+  this.render('about');
+});
+
+Router.route('resume', function () {
+  this.render('resume');
+});
+
+
